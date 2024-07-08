a333c6dc3f151a20c6a3f80fa23c8112d226d790
EddieKarira <kariraeddie9@gmail.com>
2024-07-08 14:10:09 +0300

    Updated Home page

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <footer>
+        <p>&copy; 2024 heBrews Coffee Shop. All rights reserved.</p>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,25 +1,16 @@
+<?php include 'header.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8" />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
-    <!-- Compiled and minified CSS -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
-    />
-    <!-- Material icons -->
-
-    <link
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet"
-    />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>Home</title>
+    <title>heBrews Coffee Shop</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+    <div class="main-content">
+        <h1>Welcome to heBrews</h1>
+        <p>Your perfect spot for the finest coffee and a cozy atmosphere.</p>
+        <!-- Add more content here as needed -->
+    </div>
+<?php include 'footer.php'; ?>
 </body>
 </html>
