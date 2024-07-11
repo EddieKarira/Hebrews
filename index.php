17528f3386990ec04fc84b215fe82229b8adb3d2
EddieKarira <kariraeddie9@gmail.com>
2024-07-11 13:15:24 +0300

    Linked JS to index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <title>heBrews Coffee Shop</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src = "website.js"></script>
 </head>
 <body>
     <!-- Slideshow container -->
