db05d7959733f114b11084f420bbaa2a5090f45f
EddieKarira <kariraeddie9@gmail.com>
2024-07-11 15:06:39 +0300

    Updated index page

--- index.php
+++ index.php
@@ -5,86 +5,38 @@
     <meta charset="UTF-8">
     <title>heBrews Coffee Shop</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script>
-        let slideIndex = 0;
-        showSlides();
-
-        function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-            setTimeout(showSlides, 2000); // Change image every 2 seconds
-        }
-
-        function plusSlides(n) {
-            showManualSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-            showManualSlides(slideIndex = n);
-        }
-
-        function showManualSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-        }
-    </script>
+    
 </head>
 <body>
-    <!-- Slideshow container -->
-    <div class="slideshow-container">
-        <!-- Full-width images with number and caption text -->
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="images/img1.jpg" class="slide-image">
-            <div class="text">Caption Text</div>
+<div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/img1.jpg" alt="Denim Jeans" style="width:100%">
+                <h1>Cappucino</h1>
+                <p class="price">$19.99</p>
+                <p>Get your day started with a delicious Cappucino</p>
+                <p><button>Add to Cart</button></p>
+              </div>
         </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="images/img2.jpg" class="slide-image">
-            <div class="text">Caption Two</div>
+        <div class="column">
+            <div class="card">
+                <img src="images/img2.jpg" alt="Denim Jeans" style="width:100%">
+                <h1>Cappucino</h1>
+                <p class="price">$19.99</p>
+                <p>Get your day started with a delicious Cappucino</p>
+                <p><button>Add to Cart</button></p>
+              </div>
         </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="images/img3.jpg" class="slide-image">
-            <div class="text">Caption Three</div>
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Denim Jeans" style="width:100%">
+                <h1>Cappucino</h1>
+                <p class="price">$19.99</p>
+                <p>Get your day started with a delicious Cappucino</p>
+                <p><button>Add to Cart</button></p>
+              </div>
         </div>
-
-        <!-- Next and previous buttons -->
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br>
-
-    <!-- The dots/circles -->
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
+      </div>
 
     <?php include 'footer.php'; ?>
 </body>
